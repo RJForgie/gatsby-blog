df38c7bb224ae05d87d5fdcf01fcf32875867fe3
Ryan Forgie <ryan.j.forgie@gmail.com>
2018-07-16 21:45:44 +0100

    update styling and content

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,30 +2,26 @@ import React from 'react'
 import Link from 'gatsby-link'
 
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
+          <span className="has-text-primary">
+            Ryan Forgie
+          </span>
         </Link>
       </div>
       <div className="navbar-start">
         <Link className="navbar-item" to="/about">
           About
         </Link>
-        <Link className="navbar-item" to="/products">
-          Products
-        </Link>
       </div>
       <div className="navbar-end">
         <a
           className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
+          href="https://github.com/RJForgie"
           target="_blank"
           rel="noopener noreferrer"
         >
